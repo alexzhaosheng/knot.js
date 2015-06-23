bcc32d706bb032fd40fe7e9b67f17863c7e33135
Sheng Zhao <woodheadz@gmail.com>
2015-06-23 14:02:59 +1000

    added: alias for html access pointers

--- src/core/AddonHTMLAPProvider.js
+++ src/core/AddonHTMLAPProvider.js
@@ -34,6 +34,125 @@
         delete node.__knot.dataContext;
     }
 
+    var _alias = {
+        "text":{
+            "input.text":"value",
+            "textarea":"value",
+            "option":"text",
+            "*":"textContent"
+        }
+    };
+
+    function getKeyForTarget(target){
+        var key = target.tagName.toLowerCase();
+        if(key == "input"){
+            key += target.type.toLocaleLowerCase();
+        }
+        return key;
+    }
+    function getValueFromAlias(target, apDes){
+        var key = getKeyForTarget(target);
+        if(_alias[apDes][key]){
+            return target[_alias[apDes][key]];
+        }
+        else{
+            return target[_alias[apDes]["*"]];
+        }
+    }
+    function setValueFromAlias(target, apDes, value){
+        if(typeof(value) == "undefined")
+            value = "";
+
+        var key = getKeyForTarget(target);
+        if(_alias[apDes][key]){
+            target[_alias[apDes][key]] = value;
+        }
+        else{
+            target[_alias[apDes]["*"]] = value;
+        }
+    }
+
+    function setSelectOptions(target, value, options) {
+        if (!options) {
+            options = {};
+        }
+        if (!value)
+            value = [];
+        for (var i = 0; i < value.length; i++) {
+            var option = findOption(target.options, i, value[i]);
+            if (!option) {
+                option = document.createElement("option");
+                __private.HTMLKnotManager.setOnNodeDataContext(option, value[i]);
+                if (value[i]) {
+                    if (options["displayMember"]) {
+                        setupBinding(option, "text", value[i], options["displayMember"]);
+                    }
+                    else {
+                        option.text = value[i];
+                    }
+                    if (options["valueMember"]) {
+                        setupBinding(option, "value", value[i], options["valueMember"]);
+                    }
+                    else {
+                        option.value = value[i];
+                    }
+                }
+                target.options.add(option, i)
+            }
+            else {
+                if (target.options[i].indexOf(option) != i) {
+                    target.options.remove(target.options[i].indexOf(option));
+                    target.options.add(option, i);
+                }
+            }
+        }
+
+        for (var i = target.options.length - 1; i >= value.length; i--) {
+            removeAllBindings(target.options[i]);
+            target.options.remove(i);
+        }
+    }
+
+    function setClass(target, value) {
+        if (value) {
+            var forceSettingClasses = [];
+            var origClassNames = target.className.split(" ");
+            var values = value.split(" ");
+            for (var i = 0; i < values.length; i++) {
+                var v = values[i];
+                if (v[0] == "-") {
+                    v = v.substr(1).trim();
+                    if (origClassNames.indexOf(v) >= 0)
+                        origClassNames.splice(origClassNames.indexOf(v), 1);
+                }
+                else if (v[0] == "+") {
+                    v = v.substr(1).trim();
+                    if (origClassNames.indexOf(v) < 0)
+                        origClassNames.push(v);
+                }
+                else{
+                    forceSettingClasses.push(v);
+                }
+            }
+
+            //if there's any class specified as "force setting", ignore any other classes
+            if(forceSettingClasses.length > 0)
+                target.className = forceSettingClasses.join(" ");
+            else
+                target.className = origClassNames.join(" ");
+        }
+    }
+
+    function setSelectedData(target, value){
+        for(var i=0; i<target.options.length; i++){
+            if(target.options[i].__knot && target.options[i].__knot.dataContext == value){
+                target.selectedIndex = i;
+                return;
+            }
+        }
+        target.selectedIndex = -1;
+    }
+
     var AddonHTMLAPProvider={
         doesSupport:function(target, apName){
             if(apName[0] == "#"){
@@ -45,6 +164,8 @@
                 if(target.tagName.toLowerCase()=="select" && (__private.Utility.startsWith(apName,"options") || apName == "selectedData"))
                     return true;
                 else if(apName == "class")
+                    return true;
+                else if(_alias[apName])
                     return true;
             }
 
@@ -69,6 +190,9 @@
             if(__private.Utility.startsWith(apName,"options")){
                 return target.options;
             }
+            if(_alias[apName]){
+                return getValueFromAlias(target, apName);
+            }
         },
         setValue: function(target, apName, value, options){
             if(apName[0] == "#"){
@@ -77,79 +201,16 @@
             }
 
             if(apName == "selectedData"){
-                for(var i=0; i<target.options.length; i++){
-                    if(target.options[i].__knot && target.options[i].__knot.dataContext == value){
-                        target.selectedIndex = i;
-                        return;
-                    }
-                }
-                target.selectedIndex = -1;
+                setSelectedData(target, value);
             }
-            if(apName == "class"){
-                if(value){
-                    var origClassNames = target.className.split(" ");
-                    var values = value.split(" ");
-                    for(var i=0; i< values.length; i++){
-                        var v = values[i];
-                        if(v[0] == "-"){
-                            v = v.substr(1).trim();
-                            if(origClassNames.indexOf(v)>=0)
-                                origClassNames.splice(origClassNames.indexOf(v), 1);
-                        }
-                        else{
-                            if(v[0] == "+"){
-                                v = v.substr(1).trim();
-                            }
-                            if(origClassNames.indexOf(v)<0)
-                                origClassNames.push(v);
-                        }
-                    }
-                    target.className = origClassNames.join(" ");
-                }
-                else{
-                    if(value == "-")
-                        target.className = "";
-                }
+            else if(apName == "class"){
+                setClass(target, value);
             }
             else if(__private.Utility.startsWith(apName,"options")){
-                if(!options){
-                    options = {};
-                }
-                if(!value)
-                    value = [];
-                for(var i=0; i<value.length; i++){
-                    var option = findOption(target.options, i, value[i]);
-                    if(!option){
-                        option = document.createElement("option");
-                        __private.HTMLKnotManager.setOnNodeDataContext(option, value[i]);
-                        if(value[i]){
-                            if(options["displayMember"]){
-                                setupBinding(option, "text", value[i], options["displayMember"]);
-                            }
-                            else{
-                                option.text = value[i];
-                            }
-                            if(options["valueMember"]){
-                                setupBinding(option, "value", value[i], options["valueMember"]);
-                            }
-                            else{
-                                option.value = value[i];
-                            }
-                        }
-                        target.options.add(option, i)
-                    }
-                    else{
-                        if(target.options[i].indexOf(option) != i){
-                            target.options.remove(target.options[i].indexOf(option));
-                            target.options.add(option, i);
-                        }
-                    }
-                }
-
-                for(var i=target.options.length-1; i>= value.length; i--){
-                    removeAllBindings(target.options[i]);
-                    target.options.remove(i);
-                }
+                setSelectOptions(target, value, options);
+            }
+            else if(_alias[apName]){
+                setValueFromAlias(target, apName, value);
             }
         },
         doesSupportMonitoring: function(target, apName){
